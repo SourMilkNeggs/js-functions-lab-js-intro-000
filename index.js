f0b2ea4afe3fd30be6bd1ff91dfd0986f469c1c2
Abel Enrique Cruz <abelenriquecruz@gmail.com>
2018-06-09 02:07:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-happyHolidaysTo('Frank')
+  happyHolidaysTo('Frank')
   return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(name, holiday) {
-happyHolidayTo('Janice', 'Christmas')
+  happyHolidayTo('Janice', 'Christmas')
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(days, holiday) {
-holidayCountdown('25', 'Thanksgiving')
+  holidayCountdown('25', 'Thanksgiving')
   return `It's ${days} days until ${holiday}!`
 }
